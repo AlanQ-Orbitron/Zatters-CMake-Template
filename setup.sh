95a078f758f2657dc0c0f1e323dab9ff2ce1929d
AlanQ-Orbitron <alanvquin@gmail.com>
2026-09-08 20:41:31 -0700

    build_run.sh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,6 @@ PROJECT_NAME=$(basename "$PWD")
 
 # Replace Template with the project name
 sed -i "s/Template/$PROJECT_NAME/g" CMakeLists.txt
+sed -i "s/Template/$PROJECT_NAME/g" build_run.sh
 
 echo "Created project: $PROJECT_NAME"
